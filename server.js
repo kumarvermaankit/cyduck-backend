191888666c5d521e088cbede131f70822a09b8ac
kumarvermaankit <kumarvermaankit9555@gmail.com>
2022-12-06 13:43:52 +0530

    mongo fix

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ app.use(cors());
 app.use(express.json());
 
 const uri= process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology:true });
-const connection=mongoose.connection;
+
+
 try
-{
+{   
+    mongoose.connect(uri, { useNewUrlParser:true,useUnifiedTopology:true });
+    const connection=mongoose.connection;
     connection.once('open',()=>{
     console.log("MongoDB database connection established successfully");
 })
